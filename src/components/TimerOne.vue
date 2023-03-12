e5834cded51d857bee6c3dfed9f09e82c7ac3ffb
shelbymode <andrew.ananenko@modsen-software.com>
2023-03-13 02:21:24 +0300

    feat: add env test

--- src/components/TimerOne.vue
+++ src/components/TimerOne.vue
@@ -31,7 +31,7 @@ export default {
 <template>
   <div class="greetings">
     <h1 class="green">{{ currentTime }}</h1>
-    <h3>Нажмите кнопку для сохранения в базе данных! Это пока что дев ветка!</h3>
+    <h3>Нажмите кнопку для сохранения в базе данных! Я не ебу че это за ветка: {{ process.env.TYPE_ENV }} - !</h3>
     <button @click="saveTime">Сохранить время</button>
     <h3 v-if="savedTimes.length && showSavedTimes">Ранее сохраненные времена:</h3>
     <template v-if="showSavedTimes">
