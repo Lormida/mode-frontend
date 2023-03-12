c3ae511d8771c2a309f181aeaf6ea4d65c703cfc
shelbymode <andrew.ananenko@modsen-software.com>
2023-03-12 23:40:18 +0300

    feat: init commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Time from './components/TimerOne.vue'
+</script>
+
+<template>
+  <Time :show-saved-times="true" />
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  margin: 100px auto;
+}
+
+.green {
+  color: hsla(160, 100%, 37%, 1);
+}
+</style>
---
+++ src/components/TimerOne.vue
@@ -0,0 +1,88 @@
+<script>
+import moment from 'moment'
+import { startInterval, saveTime, deleteTime } from '../utils/time'
+
+export default {
+  props: {
+    showSavedTimes: Boolean,
+  },
+  data() {
+    return {
+      currentTime: moment().format('HH:mm:ss'),
+      savedTimes: [],
+    }
+  },
+  async created() {
+    this.startInterval()
+    const res = await fetch('http://localhost:5000/times')
+    const data = await res.json()
+
+    console.log('vue res', data)
+    if (data.length) this.savedTimes = data
+  },
+  methods: {
+    startInterval,
+    saveTime,
+    deleteTime,
+  },
+}
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ currentTime }}</h1>
+    <h3>Н ажмите кнопку для сохранения в базе данных!</h3>
+    <button @click="saveTime">Сохранить время</button>
+    <h3 v-if="savedTimes.length && showSavedTimes">Ранее сохраненные времена:</h3>
+    <template v-if="showSavedTimes">
+      <div v-for="savedTime in savedTimes" :key="savedTime.id" class="deleted-items">
+        <div class="deleted-item">{{ savedTime.time }}</div>
+        <button class="btn-sm bg-red" @click="() => deleteTime(savedTime.id)">Удалить</button>
+      </div>
+    </template>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 4rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 2rem;
+}
+
+button {
+  font-size: 1.5rem;
+  margin: 30px;
+  background-color: rgb(128, 184, 244);
+  border-radius: 5px;
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+}
+
+.btn-sm {
+  margin: 10px;
+  padding: 4px;
+}
+
+.bg-red {
+  background-color: rgb(184, 93, 93);
+}
+
+.deleted-items {
+  font-size: 1.7rem;
+}
+
+.deleted-item {
+  width: 150px;
+  display: inline-block;
+}
+
+.greetings {
+  text-align: center;
+}
+</style>
